9b9cc8265d5ad8f8e1db7eba989740ac1b15c48f
janhavigangan13 <janhavigangan07@gmail.com>
2022-12-19 23:50:43 +0530

    Added task list form again

--- frontend/src/Components/Task/TaskList.js
+++ frontend/src/Components/Task/TaskList.js
@@ -5,60 +5,154 @@ import './Tasks.css'
 function TaskList() {
   return (
     <>
-    {Config.isUserLoggedin ? 
+      {Config.isUserLoggedin ?
 
-    <div className="guest-container">
-      <>
-        <div className="guest-container">
-          <div className="d-flex justify-content-center">
-          <h1 className='label-heading'>Tasks
-          </h1>
-          </div>
-          <div className="row">
-              <div className="col-md-6">
-                <article className="card mb-3 room-card p-3">
-                  <div className="row no-gutters">
-                        <h3 className='para-head'><b>Task Name: </b>
-                        </h3>
-                        <div className="g-para-text col-5">
-                        <p>
-                          <b>Deadline : </b> 
-                        </p>
-                        <p>
-                          <b>Document Name :</b> 
-                        </p>
-                        </div>
-                        <div className="g-para-text col-5">
-                        <p>
-                          <b>Description :</b> 
-                        </p>
-                        <p>
-                          <b>Task Status :</b> 
-                        </p>
-                        </div>
-                    <aside className="col-1">
-                      <div>
-                        <div className="d-grid gap-3 d-flex justify-content-around me-1">
-                          <a href="#">
-                            <i
-                              className="fa fa-edit fs-4 text-primary"
-                            //   onClick={() => getGuestId(guest.GuestId)}
-                            ></i>
-                          </a>
-                          <a href="#">
-                            <i
-                              className="fa fa-trash fs-4 text-danger"
-                            //   onClick={() => handleShow(guest.GuestId)}
-                            ></i>
-                          </a>
-                        </div>
-                      </div>
-                    </aside>
-                  </div>
-                </article>
+
+
+
+
+
+
+
+
+
+
+        // <div className="task-container">
+        //   <>
+        //     <div className="guest-container">
+        //       <div className="d-flex justify-content-center">
+        //         <h1 className='label-heading'>Tasks
+        //         </h1>
+        //       </div>
+        //       <div className="row">
+        //         <div className="col-md-6">
+        //           <article className="card mb-3 task-card p-3">
+        //             <div className="row no-gutters">
+        //               <h3 className='para-head mb-4'><b>Task Name: </b>
+        //               </h3>
+        //               <p>
+        //                 <b>Description :</b>
+        //               </p>
+        //               <div className="g-para-text col-5">
+        //                 <p>
+        //                   <b>Document Name :</b>
+        //                 </p>
+        //                 <p>
+        //                   <b>Task Status :</b>
+        //                 </p>
+
+        //               </div>
+
+        //               <div className="g-para-text col-5">
+
+        //                 <p>
+        //                   <b>Deadline : </b>
+        //                 </p>
+        //               </div>
+
+        //               <aside className="col-md-1">
+                       
+        //                 <div>
+
+        //                   <div className="d-grid gap-3 d-flex justify-content-around mb-5 me-1">
+        //                     <a href="#">
+        //                       <i
+        //                         className="fa fa-edit fs-4 text-primary"
+        //                       //   onClick={() => getGuestId(guest.GuestId)}
+        //                       ></i>
+        //                     </a>
+        //                     <a href="#">
+        //                       <i
+        //                         className="fa fa-trash fs-4 text-danger"
+        //                       //   onClick={() => handleShow(guest.GuestId)}
+        //                       ></i>
+        //                     </a>
+
+
+        //                   </div>
+        //                   <div className='tsked-btn'>
+        //                   <a href="#" className="btn task-btn mb-2"><i class="bi bi-list-task"></i>
+        //                     &nbsp; Assign
+        //                   </a>
+        //                 </div>
+        //                   {/* <a href="#" className="btn tsk-btn me-1">
+        //                      &nbsp; Assign
+        //                   </a> */}
+        //                 </div>
+        //               </aside>
+        //             </div>
+        //           </article>
+        //         </div>
+
+
+
+        <div className="task-container">
+          <>
+            <div className="guest-container">
+              <div className="d-flex justify-content-center">
+                <h1 className='label-heading'>Tasks
+                </h1>
               </div>
-            
-                  {/* <Modal show={show} onHide={handleClose}>
+              <div className="row">
+                <div className="col-sm-6">
+                  <article className="card mb-3 task-card p-3">
+                    <div className="row no-gutters">
+                      <div className="col-md-8">
+                        <div className="ms-2">
+                          <h3 className='para-head mb-4'><b>Task Name: </b>
+                          </h3>
+                          <p>
+                            <b>Description :</b>
+                          </p>
+                          <div className="g-para-text col-5">
+                            <p>
+                              <b>Document Name :</b>
+                            </p>
+                            <p>
+                              <b>Task Status :</b>
+                            </p>
+                            <p>
+                          <b>Deadline : </b>
+                        </p>
+
+
+                      </div>
+                      {/* <div className="g-para-text col-md-5">
+                        <p>
+                          <b>Deadline : </b>
+                        </p>
+                      </div> */}
+                      </div>
+                      </div>
+
+                      <aside className="col-md-3">
+                          <div className="aside-icon d-grid gap-3 d-flex justify-content-around me-1">
+                            <a href="#">
+                              <i
+                                className="fa fa-edit fs-4 text-primary"
+                              //   onClick={() => getGuestId(guest.GuestId)}
+                              ></i>
+                            </a>
+                            <a href="#">
+                              <i
+                                className="fa fa-trash fs-4 text-danger"
+                              //   onClick={() => handleShow(guest.GuestId)}
+                              ></i>
+                            </a>
+                          </div>
+                          <div className='aside-btn'>
+                          <a href="#" className="btn tsk-btn mb-2"><i class="fas fa-tasks"></i>
+                            &nbsp; Assign
+                          </a>
+                          </div>
+
+                      </aside>
+                    </div>
+                  </article>
+                </div>
+
+
+                {/* <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title> <i className="fa fa-trash-o fa-1x centered" aria-hidden="true" style={{color:"red"}}></i> Delete Guests</Modal.Title>
         </Modal.Header>
@@ -76,12 +170,12 @@ function TaskList() {
           </Button>
         </Modal.Footer>
       </Modal> */}
-          </div>
+              </div>
+            </div>
+          </>
         </div>
-      </>
-    </div>
-     :<><center><h1 className="label-heading " style={{color:"black"}}> Please Login to Access This Page </h1></center></> }
-     </>
+        : <><center><h1 className="label-heading " style={{ color: "black" }}> Please Login to Access This Page </h1></center></>}
+    </>
   );
 }
 
